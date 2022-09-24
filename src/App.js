ffad931feb8890f7106369956b0abd7b5649ab08
Murshed Majdy Qatoseh <morshedmorahed01@gmail.com>
2022-09-25 01:10:09 +0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class APP extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj:{},
+      show:false
+    
+    };
+  }
+  handleSubmit=(e)=>{
+    e.preventDefault()
+let q=e.target.name.value
+let url=`http://localhost:3007/weather?q=${q}`
+axios.get(url).then(ele=>{
+ let data=ele.data
+  this.setState({
+    obj:data,
+    show:true
+  })
+  console.log(data)
+})
+  }
+
+  
+
+  render() {
+    return(
+    <>
+      <Form onSubmit={this.handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control id="name" type="text" placeholder="Enter cityname" />
+       
+      </Form.Group>
+
+      
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    {this.state.show&&
+    <>
+    <h3>The Weather is: {this.state.obj.weather}</h3>
+    <h3>max temp for today: {this.state.obj.maxTemp}</h3>
+    <h3>min temp for today: {this.state.obj.minTemp}</h3>
+    </>}
+    
+    </>
+    )
+    
+  }
 }
-
-export default App;
+export default APP;
---
+++ src/weather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'
+
+class weather extends React.Component{
+
+    render(){
+        return(
+            this.props.weather.map((day, index)=>(
+              
+                <div id='labData' key={index}>
+                    <p>day: {day.date}</p>
+                    <p>description: {day.description} </p>
+                    <p>low_temp: {day.low_temp} </p>
+                    <p>max_temp: {day.max_temp}</p>
+                    {console.log(day)};
+                </div>
+            ))
+        )
+    }
+
+}
+
+export default weather;
